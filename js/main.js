210a03d5f93ec53078125301aa75bf7ff6323f12
Noriko Tomita <fantasia.nft22@gmail.com>
2023-09-29 13:39:55 +0900

    交差監視APIでアニメーションを付与しています。

--- js/main.js
+++ js/main.js
@@ -26,4 +26,31 @@
       hamburger.classList.toggle('active');
     });
   }
+
+  // ----------------
+  // 交差監視API
+  // ----------------
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  const options = {
+    threshold: 0.2,
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  const targets = document.querySelectorAll('.animate');
+ 
+
+  targets.forEach(target => {
+    observer.observe(target);
+  });
 }
